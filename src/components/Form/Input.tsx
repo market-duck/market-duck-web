48463897e1084da34d639f3240141ec97aab663e
jade <>
2024-04-22 23:24:43 +0900

    Feat: InputBase #1

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,74 @@
+import { ReactNode, useState } from 'react';
+import { AppRadii } from 'src/styles/tokens/AppRadii';
+import { AppSpcing } from 'src/styles/tokens/AppSpacing';
+import styled from 'styled-components';
+
+//TODO:: Color Token 완료되면 background-color, color, shadow 등 추가하기
+
+const InputWrap = styled.div.attrs<{ $focus: boolean; $error?: boolean; $disabled?: boolean }>(
+  ({ $focus, $error, $disabled }) => {
+    return {
+      className: `${$focus ? 'is-focus' : ''} ${$error ? 'is-error' : ''} ${$disabled ? 'is-diabeld' : ''}`,
+    };
+  },
+)`
+  width: 100%;
+  border-radius: ${AppRadii.M};
+  padding: ${AppSpcing.XXS};
+
+  &:hover {
+  }
+
+  &.is-focus {
+  }
+
+  &.is-error {
+  }
+
+  &.is-disabled {
+  }
+`;
+
+const InputContent = styled.div`
+  width: 100%;
+`;
+
+export const Input = ({
+  value,
+  changeHandler,
+
+  isDisabled,
+  isError,
+
+  prefix = null,
+  suffix = null,
+  caption,
+}: {
+  value: string;
+  changeHandler: React.ChangeEventHandler<HTMLInputElement>;
+  isDisabled?: boolean;
+  isError?: boolean;
+  prefix?: ReactNode;
+  suffix?: ReactNode;
+  caption?: string;
+}) => {
+  const [isFocus, setIsFocus] = useState<boolean>(false);
+
+  const onFocus = () => {
+    setIsFocus(true);
+  };
+  const onBlur = () => {
+    setIsFocus(false);
+  };
+
+  return (
+    <InputWrap $focus={isFocus} $error={isError} $disabled={isDisabled}>
+      <InputContent>
+        {prefix}
+        <input disabled={isDisabled} value={value} onChange={changeHandler} onFocus={onFocus} onBlur={onBlur} />
+        {suffix}
+      </InputContent>
+      {caption && <p>{caption}</p>}
+    </InputWrap>
+  );
+};
