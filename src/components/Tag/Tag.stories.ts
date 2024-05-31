8efe69549735feca6a2572e5af4bf33eb539ddae
ninefloor <less0805@gmail.com>
2024-05-31 21:42:57 +0900

    feat: add Tag story #12
    Co-authored-by: Jade <ansmeer008@users.noreply.github.com>

---
+++ src/components/Tag/Tag.stories.ts
@@ -0,0 +1,56 @@
+import { Tag } from '@market-duck/components/Tag/Tag';
+import { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+const meta = {
+  title: 'UI Component/Tag', //* Document, Tree 구조가 됨
+  component: Tag, //* 실제 작성한 컴포넌트
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: {
+    onDelete: fn(() => {
+      console.log('delete!');
+    }),
+  },
+} satisfies Meta<typeof Tag>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    color: 'primary',
+    text: 'primary',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    color: 'secondary',
+    text: 'secondary',
+  },
+};
+
+export const Info: Story = {
+  args: {
+    color: 'info',
+    text: 'info',
+  },
+};
+
+export const Error: Story = {
+  args: {
+    color: 'error',
+    text: 'error',
+  },
+};
+
+export const Caution: Story = {
+  args: {
+    color: 'caution',
+    text: 'caution',
+  },
+};
