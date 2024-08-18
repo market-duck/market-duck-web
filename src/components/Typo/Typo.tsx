bcd35c4b733ad9854eecb5539dec434966f9f325
ninefloor <less0805@gmail.com>
2024-08-18 14:53:15 +0900

    fix: fix Typo extends html attribute

--- src/components/Typo/Typo.tsx
+++ src/components/Typo/Typo.tsx
@@ -1,6 +1,6 @@
-import { RuleSet, styled } from 'styled-components';
-import { createElement, ReactNode } from 'react';
+import { createElement, HTMLAttributes, ReactNode } from 'react';
 import { AppTypo, AppTypoKey } from 'src/styles/tokens/AppTypo';
+import { RuleSet, styled } from 'styled-components';
 
 export const DynamicTypography = styled(({ tag, children, ...props }: { tag: 'span' | 'p'; children: ReactNode }) =>
   createElement(tag, props, children),
@@ -8,14 +8,23 @@ export const DynamicTypography = styled(({ tag, children, ...props }: { tag: 'sp
   ${(props) => props.type}
 `;
 
-interface TypoProps {
+interface OriginTypeProps {
   tag?: 'span' | 'p';
   type?: AppTypoKey;
-  children: string | ReactNode;
   className?: string;
 }
 
-export const Typo = ({ tag = 'span', type = 'BODY_MD', children, ...props }: TypoProps) => {
+interface PTypeProps extends OriginTypeProps, HTMLAttributes<HTMLParagraphElement> {
+  tag: 'p';
+}
+
+interface SpanTypeProps extends OriginTypeProps, HTMLAttributes<HTMLSpanElement> {
+  tag: 'span';
+}
+
+type TypeProps = PTypeProps | SpanTypeProps;
+
+export const Typo = ({ tag = 'span', type = 'BODY_MD', children, ...props }: TypeProps) => {
   return (
     <DynamicTypography tag={tag} type={AppTypo[type]} {...props}>
       {children}
