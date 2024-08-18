0b4cf2eb4570acf38b9c8264d6b9b0cf11b0b203
ninefloor <less0805@gmail.com>
2024-08-18 15:04:02 +0900

    feat: create Login page #32 #37

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,11 +1,12 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { Home } from '@market-duck/pages/home/Home.page';
-import { Read } from '@market-duck/pages/feed/Read.page';
-import { AppLayout } from 'src/layout/AppLayout';
 import { Alert } from '@market-duck/pages/alert/Alert.page';
-import { Mypage } from '@market-duck/pages/myPage/MyPage.page';
 import { Create } from '@market-duck/pages/feed/Create.page';
 import { Edit } from '@market-duck/pages/feed/Edit.page';
+import { Read } from '@market-duck/pages/feed/Read.page';
+import { Home } from '@market-duck/pages/home/Home.page';
+import { Login } from '@market-duck/pages/login/Login.page';
+import { Mypage } from '@market-duck/pages/myPage/MyPage.page';
+import { createBrowserRouter } from 'react-router-dom';
+import { AppLayout } from 'src/layout/AppLayout';
 
 export const appRouter = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ export const appRouter = createBrowserRouter([
       {
         path: '/',
         Component: Home,
+      },
+      {
+        path: 'login',
+        Component: Login,
       },
       {
         path: 'alert',
