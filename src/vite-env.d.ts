3352c144ee16b635049e4d7deb382c2d321727f4
ninefloor <less0805@gmail.com>
2024-09-29 14:38:57 +0900

    chore: add svgr package for svg component #43

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
 
 //https://ko.vitejs.dev/guide/env-and-mode.html#intellisense-for-typescript
 declare type ValueOfType<T> = T[keyof T];
