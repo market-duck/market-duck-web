ff7934f1df03d7d9dba4d18d18932850f5706d52
ninefloor <less0805@gmail.com>
2024-11-23 18:31:45 +0900

    feat: add and modify type, util function #54

---
+++ src/types/category.ts
@@ -0,0 +1,1 @@
+export type CategoryType = 'GOODS' | 'GENRE';
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,17 +1,10 @@
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+dayjs.locale('ko');
+
 export const getTimeDiff = (date: Date) => {
-  const now = new Date();
-
-  const seconds = Math.floor((now.getTime() - date.getTime()) / 1000);
-  if (seconds < 60) return '방금 전';
-
-  const minutes = seconds / 60;
-  if (minutes < 60) return `${Math.floor(minutes)}분 전`;
-
-  const hours = minutes / 60;
-  if (hours < 24) return `${Math.floor(hours)}시간 전`;
-
-  const days = hours / 24;
-  if (days < 7) return `${Math.floor(days)}일 전`;
-
-  return date.toLocaleDateString();
+  return dayjs(date).fromNow();
 };
