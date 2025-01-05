d0b3e01cc987164dfe5ab9f47bfd373cc1ef1733
ninefloor <less0805@gmail.com>
2025-01-05 12:54:27 +0900

    fix: create feedModel, categoryModel

--- src/apis/feedAPI.ts
+++ src/apis/feedAPI.ts
@@ -22,12 +22,14 @@ class FeedAPI {
       pageInfo,
     };
   }
+
   async getFeedDetail({ feedId }: { feedId: number }) {
     const {
       data: { data },
     } = await openFetchClient.get<IAPIResponse<IFeedDetailModelData>>(`/feed/detail/${feedId}`);
     return FeedDetailModel.fromJson(data);
   }
+
   //새롭게 업로드시 & 피드 수정시에 추가 업로드도 가능
   async uploadFeedImages({ feedId, imgList }: { feedId: number; imgList: File[] }) {
     const file = new FormData();
@@ -35,25 +37,25 @@ class FeedAPI {
     const { status } = await fetchClient.post(`feed/image/${feedId}`, file);
     return status <= 299 ? NetworkResultType.success : NetworkResultType.fail;
   }
+
   //특정 피드의 업로드된 이미지 리스트에서 특정 인덱스에 해당하는 이미지를 제거한다
   async deleteFeedImages({ feedId, index }: { feedId: number; index: number }) {
     const { status } = await fetchClient.patch(`/feed/${feedId}/${index}}`, { feedId, index });
-
     return status <= 299 ? NetworkResultType.success : NetworkResultType.fail;
   }
+
   async createFeed(feedData: ReqFeedModelData) {
     const { status } = await fetchClient.post('feed', feedData);
-
     return status <= 299 ? NetworkResultType.success : NetworkResultType.fail;
   }
+
   async editFeed({ feedId, feedData }: { feedId: number; feedData: ReqFeedModelData }) {
     const { status } = await fetchClient.patch(`feed/${feedId}`, feedData);
-
     return status <= 299 ? NetworkResultType.success : NetworkResultType.fail;
   }
+
   async deleteFeed({ feedId }: { feedId: number }) {
     const { status } = await fetchClient.delete(`feed/${feedId}`);
-
     return status <= 299 ? NetworkResultType.success : NetworkResultType.fail;
   }
 }
---
+++ src/apis/models/categoryModel.ts
@@ -0,0 +1,21 @@
+import { CategoryType } from '@market-duck/types/category';
+
+export interface ICategoryModelData {
+  categoryId: number;
+  categoryName: string;
+  categoryType: CategoryType;
+}
+
+export class CategoryModel {
+  categoryId: number;
+  categoryName: string;
+  categoryType: CategoryType;
+  constructor(data: ICategoryModelData) {
+    this.categoryId = data.categoryId;
+    this.categoryName = data.categoryName;
+    this.categoryType = data.categoryType;
+  }
+  static fromJson(data: ICategoryModelData) {
+    return new CategoryModel(data);
+  }
+}
