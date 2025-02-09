fcb327a54d76046a201db41b46ddee7675213e2d
ninefloor <less0805@gmail.com>
2025-02-09 19:22:54 +0900

    fix: modify onboarding and edit user info

--- src/atoms/user.atom.ts
+++ src/atoms/user.atom.ts
@@ -4,4 +4,13 @@ import { atom } from 'recoil';
 export const userDataAtom = atom<UserModel | null>({
   key: 'userData',
   default: null,
+  effects: [
+    (param) => {
+      if (param.trigger === 'set') {
+        param.onSet((newValue) => {
+          console.log(newValue);
+        });
+      }
+    },
+  ],
 });
