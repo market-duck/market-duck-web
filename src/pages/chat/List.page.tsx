4193f918bbae88a755ec7d1f55e51c7eae9df70e
Jade <neederjade@gmail.com>
2025-05-01 16:43:09 +0900

    feat: add not found chat list

--- src/pages/chat/List.page.tsx
+++ src/pages/chat/List.page.tsx
@@ -6,11 +6,22 @@ import { useQuery } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { ChatListItem } from './components/ChatListItem';
+import NotFoundImage from '@market-duck/assets/images/notFound.svg?react';
+import { Typo } from '@market-duck/components/Typo/Typo';
+import { AppSemanticColor } from 'src/styles/tokens/AppColor';
+import { AppSpcing } from 'src/styles/tokens/AppSpacing';
 
 const Wrap = styled(AppGutter)`
   position: relative;
   display: flex;
   flex-direction: column;
+
+  &.empty {
+    height: 100dvh;
+    justify-content: center;
+    align-items: center;
+    gap: ${AppSpcing.XXXL};
+  }
 `;
 
 const dummy = [
@@ -52,21 +63,30 @@ export const ChatList = () => {
   return (
     <>
       <NavigationTop leftButtonIconType="back" title="채팅 목록" onLeftClick={() => navigate('/')} />
-      <Wrap>
-        {chatRooms?.map((room) => {
-          return (
-            <ChatListItem
-              key={room.chatRoomId}
-              imgUrl={room.receiver.profileImageUrl}
-              id={room.chatRoomId}
-              name={room.receiver.nickname}
-              lastMessage={room.recentMessages[room.recentMessages.length - 1].content}
-              noReadCount={room.unreadCount}
-              lastViewDate={getTimeDiff(room.recentMessages[room.recentMessages.length - 1].createdAt)}
-            />
-          );
-        })}
-      </Wrap>
+      {chatRooms && chatRooms.length ? (
+        <Wrap>
+          {chatRooms?.map((room) => {
+            return (
+              <ChatListItem
+                key={room.chatRoomId}
+                imgUrl={room.receiver.profileImageUrl}
+                id={room.chatRoomId}
+                name={room.receiver.nickname}
+                lastMessage={room.recentMessages[room.recentMessages.length - 1].content}
+                noReadCount={room.unreadCount}
+                lastViewDate={getTimeDiff(room.recentMessages[room.recentMessages.length - 1].createdAt)}
+              />
+            );
+          })}
+        </Wrap>
+      ) : (
+        <Wrap className="empty">
+          <NotFoundImage />
+          <Typo tag="p" align="center" type="BODY_MD" weight={500} className={AppSemanticColor.TEXT_PRIMARY.color}>
+            현재 참여중인 채팅이 없습니다.
+          </Typo>
+        </Wrap>
+      )}
     </>
   );
 };
